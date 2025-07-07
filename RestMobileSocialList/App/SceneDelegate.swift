f0820c0fbeafbe829bda37173d1a58e516c22aa1
webdmitriev <webdmitriev@gmail.com>
2025-07-07 22:41:21 +0300

    Install loading installation screen

--- RestMobileSocialList/App/SceneDelegate.swift
+++ RestMobileSocialList/App/SceneDelegate.swift
@@ -14,8 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let sceneWindow = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: sceneWindow)
-        //showOnboarding()
-        tabBar()
+        showOnboarding()
+        //tabBar()
         self.window?.makeKeyAndVisible()
     }
     
